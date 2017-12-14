7c0f30bb4f43178c5c616dee108389e459bfbb37
Tobias Pleyer <tobi.pleyer@gmail.com>
2017-12-14 22:51:13 +0100

    Basic functionality of the plugin

---
+++ todo/checker.py
@@ -0,0 +1,25 @@
+import re
+
+
+class TodoChecker(object):
+    todo_re = re.compile(r'([#: ]+todo[#: ]*)|(^todo\W+)', re.IGNORECASE)
+    def __init__(self, options, path):
+        self.path = path
+        self.hasErrors = False
+        self.found = []
+
+    def check(self):
+        with open(self.path, 'r') as fp:
+            for lnum, text in enumerate(fp):
+                m = self.todo_re.search(text)
+                if m:
+                    self.found.append({'lnum': lnum, 'text': text})
+        if self.found:
+            self.hasErrors = True
+
+    def summary(self):
+        summary = "File {}\n".format(self.path)
+        summary += "Found {} TODOs\n".format(len(self.found))
+        info = "Line {lnum}: {text}"
+        summary += '\n'.join([info.format(**todo) for todo in self.found])
+        return summary
--- todo/plugin.py
+++ todo/plugin.py
@@ -1,1 +1,40 @@
 import pytest
+from os.path import relpath
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("general")
+    group.addoption(
+        '--todo', action='store_true',
+        help="Check for TODO comments in .py files")
+
+
+def pytest_collect_file(path, parent):
+    config = parent.config
+    if config.option.todo and path.ext == '.py':
+        return TodoItem(path, parent)
+
+
+class TodoError(Exception):
+    """The custom error class for TODO checks"""
+
+
+class TodoItem(pytest.Item, pytest.File):
+
+    def runtest(self):
+        checker = check_todo(self.fspath)
+        if checker.hasErrors:
+            raise TodoError(checker.summary())
+
+    def repr_failure(self, excinfo):
+        if excinfo.errisinstance(TodoError):
+            return excinfo.value.args[0]
+        return super(TodoItem, self).repr_failure(excinfo)
+
+
+def check_todo(path):
+    options = {}
+    from todo.checker import TodoChecker
+    checker = TodoChecker(options, relpath(path))
+    checker.check()
+    return checker
